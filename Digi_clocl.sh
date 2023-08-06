685d4648eec648e7e9235aa81bc5e644d0a39cef
suraj kumar jha <69684103+surajjha97@users.noreply.github.com>
2023-08-06 14:10:52 +0530

    This is a digital clock
    this clock will run until you will close the terminal

---
+++ Digi_clocl.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+#Black BLACK= $'\Esc[30m'
+#Red DARK_RED= $'\Esc[31m'
+#Green DARK_GREEN= $'\Esc[32m'
+#Yellow DARK_YELLOW= $'\Esc[33m'
+#Blue DARK_BLUE=$'\Esc[34m'
+#Magenta DARK_MAGENTA= $'\Esc[35m'
+#Cyan DARK_CYAN= $'\Esc[36m'
+#
+#
+red=$'\e[1;31m'
+
+while true
+do 
+	clear
+	echo $red $(date +%T)
+	sleep 1s
+	
+done
+
+
+
